4b7f2ba5ee379677538f0742340baa5d09c7d590
Hayder Ali <hali@spartaglobal.com>
2015-11-24 15:29:15 +0000

    Tumblr script working with working a yaml file

--- Gemfile
+++ Gemfile
@@ -1,3 +1,3 @@
 source 'https://rubygems.org'
 
-gem 'selenium-webdriver','>2.48.1', :require => 'selenium-webdriver'
+gem 'selenium-webdriver'
--- tumblrpost.rb
+++ tumblrpost.rb
@@ -1,31 +1,36 @@
 #script for tumblr
-require 'selenium-webdriver'
 
+require 'YAML'
+file = YAML::load_file("./user_credential.yml")
+
+require 'selenium-webdriver'
 browser = Selenium:: WebDriver.for :firefox
 browser.get "http://www.tumblr.com/login"
 
-enter_email = browser.find_element id:"signup_email"
-enter_email.send_keys "hayder.ali92@gmail.com"
-enter_pass = browser.find_element id: "signup_password"
-enter_pass.send_keys "banana11"
-enter_pass.submit
+file["credential"].each do |credential|
+  enter_email = browser.find_element(id:"signup_email") 
+  enter_email.send_keys credential["username"]
+  enter_pass = browser.find_element(id:"signup_password") 
+  enter_pass.send_keys credential["password"]
+  enter_pass.submit
+end
 
 text_post = browser.find_element id: "new_post_label_text"
 text_post.click
 
 message = browser.find_element class: "editor-richtext"
 message.send_keys "test message"
+
 post_button = browser.find_element class: "post-form--save-button"
 post_button.click
 
-browser.get "http://deepesthologramtyphoon.tumblr.com/"
+dashboard_check = browser.find_elements(class: "post_body")
+dashboard_check.each_with_index do |message, post|
 
-
-# if wait.until do
-# browser.find_element(tag_name: "p").text.match /test message/
-  # puts "Test Passed! Found THE MESSAGE !!!!" 
-# else
-  # puts "Can't Found"
-# end
-
+  if message.text.match /test message/
+    puts  "Post #{post+1}: The message exists !!!"
+  else  
+    puts "Post #{post+1}: The message doesn't exist :("
+  end
+end 
 
